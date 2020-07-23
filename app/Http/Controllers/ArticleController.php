5d5925fc858b7584b48ae425ddf361cb338403c8
div-antk <51283468+owaii@users.noreply.github.com>
2020-07-23 21:27:14 +0900

    コントローラの編集

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -2,25 +2,18 @@
 
 namespace App\Http\Controllers;
 
+// モデルを使う
+use App\Models\Article;
 use Illuminate\Http\Request;
+
 
 class ArticleController extends Controller
 {
     public function index()
     {
-        $articles = [
-            // 型キャスト。配列の手前に(object)と記述することで、配列がオブジェクト型に変換されるß
-            (object) [
-                'id' => 1,
-                'title' => 'タイトル1',
-                'body' => '孫文',
-                'created_at' => now(),
-                'user' => (object) [
-                    'id' => 1,
-                    'name' => 'たくや',
-                ],
-            ],
-        ];
+        // allメソッドはモデルが持つクラスメソッド
+        $articles = Article::all()->sortByDesc('created_at');
+
         // viewメソッド
         // 第一引数はビューファイルの指定
         // 第二引数はビューファイルに渡す変数の名称と、その変数の値を連想配列で指定
--- app/Models/Article.php
+++ app/Models/Article.php
@@ -5,11 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-
 class Article extends Model
 {
     public function user(): BelongsTo
     {
+        // $thisはArticleクラスのインスタンス自身を指している
         return $this->belongsTo('App\Models\User');
     }
 }
