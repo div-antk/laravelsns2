d1b377f71bf09ef769f8a7e764224469f2109801
div-antk <51283468+owaii@users.noreply.github.com>
2020-07-25 14:24:04 +0900

    記事投稿画面のルート追加

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,7 @@
 Auth::routes();
 
 // 引数2つ。URLとどのコントローラでなんのメソッドを実行するか
-Route::get('/', 'ArticleController@index');
+Route::get('/', 'ArticleController@index')->name('articles.index');
+
+// 登録画面表示
+Route::get('articles/create', 'ArticleController@create')->name('articles.create');
