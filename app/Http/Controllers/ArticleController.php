205852ad68209ed50e81384b58ed7e8eee896b75
div-antk <51283468+owaii@users.noreply.github.com>
2020-07-28 08:34:53 +0900

    更新処理機能の実装

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -67,4 +67,11 @@ class ArticleController extends Controller
     {
         return view('articles.edit')->with(['article' => $article]);
     }
+
+    public function update(ArticleRequest $request, Article $article)
+    {
+        $article->fill($request->all())->save();
+
+        return redirect()->route('articles.index');
+    }
 }
