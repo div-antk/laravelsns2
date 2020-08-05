d4c4a375464846164cd2a1dd97aa77b26253bf47
div-antk <51283468+owaii@users.noreply.github.com>
2020-08-05 21:03:51 +0900

    修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,18 +4,17 @@ namespace App\Http\Controllers;
 // use App\Repositories\Article\ArticleRepositoryInterface;
 use App\Repositories\User\UserRepositoryInterface;
 
-
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {    
-    protected $UserRepository;
+    protected $user;
 
-    public function __construct(UserRepositoryInterface $UserRepository)
+    public function __construct(UserRepositoryInterface $userRepository)
     {
         // リポジトリパターン
-        $this->userRepository = $user;
+        $this->userRepository = $userRepository;
         
         // ポリシーの適用
         // $this->authorizeResource(Article::class, 'article');
@@ -26,7 +25,7 @@ class UserController extends Controller
         // whereメソッドで$nameと一致する名前を持つユーザーモデルをコレクションで取得する
         // ただし取得したのはユーザーモデル1件が入ったコレクション（配列）なので、そのままではモデルとして取り扱うことができない
         // なのでfirstメソッドを使って最初の1件だけを取得して変数に代入している
-        $user = $this->user->getUser($name);
+        $user = $this->userRepository->getUser($name);
 
         $articles = $user->articles->sortByDesc('create_at');
 
