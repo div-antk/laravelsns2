936aeafe64f34be1a52388ddef36e9c4cb797ac6
div-antk <51283468+owaii@users.noreply.github.com>
2020-08-18 23:00:36 +0900

    テーブルの作成

--- database/migrations/2020_08_17_140022_add_two_factor_auth_fields_to_users.php
+++ database/migrations/2020_08_17_140022_add_two_factor_auth_fields_to_users.php
@@ -15,7 +15,7 @@ class AddTwoFactorAuthFieldsToUsers extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             $table->string('tfa_token')
-                ->nullabel()
+                ->nullable()
                 ->after('remember_token');
             $table->dateTime('tfa_expiration')
                 ->nullable()
